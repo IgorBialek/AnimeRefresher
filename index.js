2cd7c0771b604b9a474bb5d52a51caf3a7144fc4
IgorBialek <igorbialek0@gmail.com>
2022-12-15 00:01:30 +0100

    Environment Switch To Render

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const express = require("express");
 require("dotenv").config();
 
@@ -6,29 +7,11 @@ const app = express();
 
 const port = process.env.port || 3001;
 
-let interval = null;
-let iterator = 1;
-
 app.get("/", async (req, res) => {
-  if (interval) {
-    iterator = 1;
-    clearInterval(interval);
-  }
-
   let startTime = new Date().getTime();
   console.log(`Request ${new Date(startTime).toLocaleTimeString()}`);
 
-  interval = setInterval(() => {
-    if (iterator > 1000) {
-      iterator = 1;
-      clearInterval(interval);
-    }
-
-    console.log(iterator);
-    iterator++;
-  }, 1000);
-
-  //await processData();
+  processData();
 
   let takenTime = new Date().getTime() - startTime;
   let timeString = `${new Date(takenTime).getMinutes()}:${new Date(
